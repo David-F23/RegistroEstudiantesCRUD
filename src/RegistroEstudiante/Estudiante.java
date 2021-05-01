b1e84f39a1ca60e6baba6e305eff910338d5c8bc
David E. Flores <floresdavide0@gmail.com>
2021-05-01 10:35:50 -0600

    Se creo la clase estudiante

---
+++ src/RegistroEstudiante/Estudiante.java
@@ -0,0 +1,50 @@
+package RegistroEstudiante;
+
+public class Estudiante {
+    
+    int id_estudainte;
+    String carnet_estudiante;
+    String nom_estudiante;
+    String ape_estudiante;
+    int edad_estudante;
+
+    public int getId_estudainte() {
+        return id_estudainte;
+    }
+
+    public void setId_estudainte(int id_estudainte) {
+        this.id_estudainte = id_estudainte;
+    }
+
+    public String getCarnet_estudiante() {
+        return carnet_estudiante;
+    }
+
+    public void setCarnet_estudiante(String carnet_estudiante) {
+        this.carnet_estudiante = carnet_estudiante;
+    }
+
+    public String getNom_estudiante() {
+        return nom_estudiante;
+    }
+
+    public void setNom_estudiante(String nom_estudiante) {
+        this.nom_estudiante = nom_estudiante;
+    }
+
+    public String getApe_estudiante() {
+        return ape_estudiante;
+    }
+
+    public void setApe_estudiante(String ape_estudiante) {
+        this.ape_estudiante = ape_estudiante;
+    }
+
+    public int getEdad_estudante() {
+        return edad_estudante;
+    }
+
+    public void setEdad_estudante(int edad_estudante) {
+        this.edad_estudante = edad_estudante;
+    }
+}
